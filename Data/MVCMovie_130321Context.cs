c8542ba84e2f96ae959f4010d10f51bbefb4c093
AdiliaZ <65673503+AdiliaZ@users.noreply.github.com>
2021-03-20 08:58:51 +0300

    20.03.21

---
+++ Data/MVCMovie_130321Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCMovie_130321.Models;
+
+namespace MVCMovie_130321.Data
+{
+    public class MVCMovie_130321Context : DbContext
+    {
+        public MVCMovie_130321Context (DbContextOptions<MVCMovie_130321Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCMovie_130321.Models.Movie> Movie { get; set; }
+    }
+}
